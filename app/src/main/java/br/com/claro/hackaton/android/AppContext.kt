e51a99792c763d4e4dac15b11910e51d0d8732e4
Leandro Andrade <leandro.andrade@globalhitss.com.br>
2018-02-22 19:30:28 -0300

    add timber, appcontext

---
+++ app/src/main/java/br/com/claro/hackaton/android/AppContext.kt
@@ -0,0 +1,39 @@
+package br.com.claro.hackaton.android
+
+import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
+import timber.log.Timber
+
+/**
+ * Created by Leandro on 22/02/2018.
+ */
+class AppContext : Application() {
+
+    companion object {
+        lateinit var instance: AppContext private set
+
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        initializeTimezone()
+        initializeTimber()
+        initializeApiModules()
+    }
+
+    private fun initializeTimezone() {
+        AndroidThreeTen.init(this)
+    }
+
+    private fun initializeTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+    private fun initializeApiModules() {
+        //Initialize ApiModule Singleton
+//        GitHubApiModule.setRetrofit()
+    }
+}
