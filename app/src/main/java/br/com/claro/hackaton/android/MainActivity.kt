3ec5ecf0bc0785f4f02ec307928dd2f89cbaa483
Leandro Andrade <leandro.andrade@globalhitss.com.br>
2018-02-22 17:52:39 -0300

    solve conflict dependencies

--- app/src/main/java/br/com/claro/hackaton/android/MainActivity.kt
+++ app/src/main/java/br/com/claro/hackaton/android/MainActivity.kt
@@ -1,12 +1,45 @@
 package br.com.claro.hackaton.android
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import android.nfc.NfcAdapter
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.jetbrains.anko.toast
 
+
+/**
+ * Created by Leandro on 22/02/2018.
+ */
 class MainActivity : AppCompatActivity() {
+
+    private var mNfcAdapter: NfcAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        toast(NFCUtil.retrieveNFCMessage(this.intent))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mNfcAdapter?.let {
+            NFCUtil.enableNFCInForeground(it, this, javaClass)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mNfcAdapter?.let {
+            NFCUtil.disableNFCInForeground(it, this)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+//        val messageWrittenSuccessfully = NFCUtil.createNFCMessage(messageEditText.text.toString(), intent)
+//        toast(messageWrittenSuccessfully.ifElse("Successful Written to Tag", "Something When wrong Try Again"))
     }
 }
+
+fun <T> Boolean.ifElse(primaryResult: T, secondaryResult: T) = if (this) primaryResult else secondaryResult
